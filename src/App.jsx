a9d939cad68e22acc645c45bc4d35d61a9bc090c
Dominic-N1 <vladimirndsm@gmail.com>
2022-10-19 18:09:32 +0700

    show-hide Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    return setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    return setCartIsShown(false);
+  };
+
   return (
     <React.Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -9,7 +9,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <figure className={classes["main-image"]}>
         <figcaption>Meals</figcaption>
