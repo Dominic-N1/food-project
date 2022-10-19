9ef3d0eb7ffea1af53cc8c2642be671a1073ce6b
Dominic-N1 <vladimirndsm@gmail.com>
2022-10-19 17:08:21 +0700

    add modal & cart

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import CartItem from "./CartItem";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = [].map((item) => (
+    <CartItem
+      key={item.id}
+      name={item.name}
+      amount={item.amount}
+      prise={item.prise}
+      onAdd={item.onAdd}
+      onRemove={item.onRemove}
+    />
+  ));
+
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>$35.63</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
